d97ddd553f0c7975c6f4542509ed8fe4b58ed9ab
Lucas Feitosa <lucasfmfeit@gmail.com>
2024-10-04 07:56:54 -0300

    aula dia 04/10

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+let category = 23
+
 document.addEventListener("DOMContentLoaded",()=>{
     const btn = document.getElementById("selecionar")
+    const select = document.getElementById("categorias")
     btn.addEventListener("click",getQuestions)
+    select.addEventListener("change",(e)=>{
+        category = e.target.value
+    })
+    
 })
 
 async function getQuestions(){
-    const response = await fetch("https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple")
+    const response = await fetch(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=medium&type=multiple`)
     const data = await response.json()  
-    console.log(data)  
+    const results = data.results
+    const questions = results.map((item)=>{
+        return {
+            correct_answer:item.correct_answer,
+            incorrect_answers:item.incorrect_answers,
+            question:item.question
+        }
+    })
+    console.log(questions)  
 }
+
+
 
